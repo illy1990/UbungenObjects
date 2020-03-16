08b01d53b515cfa1d87267af55cead7f673ec566
illy1990 <jascha.hell@outlook.de>
2020-03-16 15:55:34 +0100

    daasdss

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,3 +136,30 @@ myMusic.forEach((title) => { document.write(myMusic.title) });
 
 myMusic.forEach((formate) => { document.write(myMusic.formate) });
 
+
+// ---------------------- Übung 6 - CodeFlow Übung lev1_8: Objects Arrays Loop Accessing ----------------------
+
+var studentData = [
+    {
+        name: "Alex",
+        age: 23,
+        coop: false,
+        address: {
+            street: "Don Valley Business Park",
+            city: "Toronto",
+            postalCode: "ONM3C3E5"
+        },
+        emails: ["alex69@gmail.com", "alex123@yahoo.com"]
+    },
+    {
+        name: "Sandra",
+        age: 22,
+        coop: true,
+        address: {
+            street: "34 Lawrence Ave",
+            city: "Toronto",
+            postalCode: "ONM3C0E5"
+        },
+        emails: ["sandra@gmail.com", "sandra@yahoo.com"]
+    }
+];
