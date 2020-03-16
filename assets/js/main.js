f428cfd069e995d2b14ce1c56e383078f7843583
illy1990 <jascha.hell@outlook.de>
2020-03-16 14:13:10 +0100

    iweuweiur

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,21 +27,13 @@ let europäischeUnion = {
     Irland: "(IE)",
     Lettland: "(LV)",
     Polen: "(PL)",
-    VereinigtesKonigreich: "(UK)"
-}
+    VereinigtesKonigreich: "(UK)",
+    BeneluxUnion: function () {
+        console.log(europäischeUnion.Belgien),
+            console.log(europäischeUnion.Niederlände),
+            console.log(europäischeUnion.Luxemburg)
+    }
 
-console.log(europäischeUnion);
-console.log(europäischeUnion.Belgien, europäischeUnion.Niederlände, europäischeUnion.Luxemburg);
-
-
-
-let BeneluxUnion = {
-    Land1: "Laender1",
-    Land2: "Laender2",
-    Land3: "Laender3"
-}
-
-console.log(BeneluxUnion)
 
 
 // europäischeUnion = europäischeUnion.push(BeneluxUnion)
@@ -49,21 +41,21 @@ console.log(BeneluxUnion)
 // Funktioniert nicht, habe keinen plan warum, internet spackt daheim, telefonieren geht auch nicht. 
 // mache nun einfach mal mit der nächsten aufgabe weiter. whatsoever
 
-console.log(europäischeUnion)
+// console.log(europäischeUnion)
 
 // ---------------------- Übung 2 - CodeFlow Übung lev1_2: Objects: ----------------------
 
 let person = {
-    name: "Peter",
-    alter: 29,
-    sagNameAlter: function () {
-        alert(alter);
+        name: "Peter",
+        alter: 29,
+        sagNameAlter: function () {
+            alert(alter);
+        }
     }
-}
 
 console.log(person.sagNameAlter);
-console.log(person.name);
-console.log(person.alter);
+    console.log(person.name);
+    console.log(person.alter);
 
 // ---------------------- Übung 3 - CodeFlow Übung lev1_4: Objects Accessing ----------------------
 
