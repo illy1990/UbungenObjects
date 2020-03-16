53e4ef02f8adab1285fd68f2a0f4ea7e186ab7b8
illy1990 <jascha.hell@outlook.de>
2020-03-16 14:09:30 +0100

    V001

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+// ---------------------- Übung 1 - Objects CodeFlow Übung lev1_1: ----------------------
+let europäischeUnion = {
+    Belgien: "(BE)",
+    Griechenland: "(EL)",
+    Litauen: "(LT)",
+    Portugal: "(PT)",
+    Bulgarien: "(BG)",
+    Spanien: "(ES)",
+    Luxemburg: "(LU)",
+    Rumänien: "(RO)",
+    Tschechien: "(CZ)",
+    Frankreich: "(FR)",
+    Ungarn: "(HU)",
+    Slowenien: "(SI)",
+    Dänemark: "(DK)",
+    Kroatien: "(HR)",
+    Malta: "(MT)",
+    Slowakei: "(SK)",
+    Deutschland: "(DE)",
+    Italien: "(IT)",
+    Niederlände: "(NL)",
+    Finnland: "(FI)",
+    Estland: "(EE)",
+    Zypern: "(CY)",
+    Österreich: "(AT)",
+    Schweden: "(SE)",
+    Irland: "(IE)",
+    Lettland: "(LV)",
+    Polen: "(PL)",
+    VereinigtesKonigreich: "(UK)"
+}
+
+console.log(europäischeUnion);
+console.log(europäischeUnion.Belgien, europäischeUnion.Niederlände, europäischeUnion.Luxemburg);
+
+
+
+let BeneluxUnion = {
+    Land1: "Laender1",
+    Land2: "Laender2",
+    Land3: "Laender3"
+}
+
+console.log(BeneluxUnion)
+
+
+// europäischeUnion = europäischeUnion.push(BeneluxUnion)
+
+// Funktioniert nicht, habe keinen plan warum, internet spackt daheim, telefonieren geht auch nicht. 
+// mache nun einfach mal mit der nächsten aufgabe weiter. whatsoever
+
+console.log(europäischeUnion)
+
+// ---------------------- Übung 2 - CodeFlow Übung lev1_2: Objects: ----------------------
+
+let person = {
+    name: "Peter",
+    alter: 29,
+    sagNameAlter: function () {
+        alert(alter);
+    }
+}
+
+console.log(person.sagNameAlter);
+console.log(person.name);
+console.log(person.alter);
+
+// ---------------------- Übung 3 - CodeFlow Übung lev1_4: Objects Accessing ----------------------
+
+
+
+
+
